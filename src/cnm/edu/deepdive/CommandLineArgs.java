0c041897f56dbb572cc4e7671637912086d332a1
finn <isaaclindland@protonmail.com>
2019-09-16 11:37:38 -0600

    changed uppercase to lowercase

--- src/cnm/edu/deepdive/CommandLineArgs.java
+++ src/cnm/edu/deepdive/CommandLineArgs.java
@@ -5,9 +5,8 @@ import java.util.Arrays;
 public class CommandLineArgs {
 
   public static void main(String[] args) {
-
     for (String arg : args) {
-      System.out.println(arg.toUpperCase());
+      System.out.println(arg);
     }
 
     System.out.println(Arrays.toString(args));
