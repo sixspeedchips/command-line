732c03be23fb15dbfdb1240855c12e5b78218a40
finn <isaaclindland@protonmail.com>
2019-09-16 10:27:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Command Line Arguments Example 
+
+
+The code in this project illustrates the consumption and processing the Java command line arguments.
+
+## Known Issues
+
+## Acknowledges
+
+
+
---
+++ src/cnm/edu/deepdive/CommandLineArgs.java
@@ -0,0 +1,13 @@
+package cnm.edu.deepdive;
+
+import java.util.Arrays;
+
+public class CommandLineArgs {
+
+  public static void main(String[] args) {
+
+    for (int i = 0; i < args.length; i++) {
+      System.out.println(args[i].toUpperCase());
+    }
+  }
+}
